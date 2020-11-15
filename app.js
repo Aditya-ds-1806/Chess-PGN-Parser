f1fa4263929f0d641e3a50cc9a5f63d2483991e6
Aditya DS <adithya18062000@gmail.com>
2020-11-15 18:18:47 +0530

    added support for {} styled annotations and NAGs

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 exports.pgn2json = function (fileName) {
     const fs = require('fs');
-    const pgn = fs.readFileSync(fileName).toString().split('\n');
+    const pgn = fs.readFileSync('sample-2.pgn').toString().split('\n');
     var game = {
         str: {},
-        moves: []
+        moves: [],
+        annotations: [],
+        nag: []
     }
     var moves;
 
-    pgn.forEach((_, i, pgn) => pgn[i] = pgn[i].replace('\r', ''));
-
+    // extract STR details and get the second part of PGN
+    pgn.forEach((_, i, pgn) => pgn[i] = pgn[i].trim().replace('\r', ''));
     for (let i = 0; i < pgn.length; i++) {
         var line = pgn[i];
         if (line[0] === '[') {
@@ -16,17 +18,54 @@ exports.pgn2json = function (fileName) {
             let value = line.substring(line.indexOf(' ') + 2, line.length - 2);
             game.str[tag] = value;
         } else if (line[0] !== '[' && line.trim() !== '') {
-            moves = pgn.slice(pgn.indexOf(line)).join(' ').split(' ');
-            moves.pop();
+            moves = pgn.slice(pgn.indexOf(line)).join(' ').trim().split('');
             break;
         }
     }
 
-    for (let i = 0; i < moves.length; i = i + 3) {
-        let move = [moves[i + 1], moves[i + 2]];
-        if (typeof moves[i + 2] === 'undefined') move.pop();
-        game.moves.push(move);
+    // get positions of commentary
+    var movPos = [];
+    var moveClone = moves.join('').split(' ');
+
+    for (let i = 1; moveClone.includes(`${i}.`); i++) {
+        movPos.push(moveClone.indexOf(`${i}.`));
     }
 
+    // extract commentary and corresponding move
+    while (moves.indexOf('{') !== -1) {
+        var commentStartPos = moves.indexOf('{');
+        var commentEndPos = moves.indexOf('}');
+        var comment = moves.splice(commentStartPos, commentEndPos - commentStartPos + 2).join('');
+        var commentIndex = moveClone.indexOf(comment.split(' ')[0]);
+        game.annotations.push({
+            moveCount: movPos.filter(ind => ind < commentIndex).length,
+            comment: comment.substring(1, comment.length - 2)
+        });
+    }
+
+    // update move positions
+    moves = moves.join('').split(' ');
+    moves.pop();
+    movPos = [];
+    for (let i = 1; moves.includes(`${i}.`); i++) {
+        movPos.push(moves.indexOf(`${i}.`));
+    }
+
+    // extract NAGs and corresponding move
+    var temp = [];
+    game.nag = moves.filter(val => val.includes('$')).map(val => {
+        var prev = 0;
+        if (temp.length) {
+            prev = temp.reverse()[0];
+        }
+        var count = movPos.filter(i => i < moves.indexOf(val, prev));
+        temp.push(count.reverse()[0]);
+        return {
+            moveCount: count.length,
+            value: val
+        }
+    });
+    moves = moves.filter(val => !val.includes('.') && !val.includes('$'));
+    game.moves = moves;
     return JSON.stringify(game);
 }
