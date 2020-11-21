2086989ddfe65d15fb2677d4c8fce46d1a0cd177
Aditya DS <adithya18062000@gmail.com>
2020-11-21 23:38:10 +0530

    code refactor

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { getCommentary, getNags, getStrAndStringifyMoves } from "./helpers";
+
 export function pgn2json(pgnText) {
     const pgn = pgnText.split('\n');
     var game = {
@@ -6,62 +8,21 @@ export function pgn2json(pgnText) {
         annotations: [],
         nag: []
     }
-    var moves;
+    var moves, moveClone, movPos = [];
+    [game.str, moves] = getStrAndStringifyMoves(pgn);
 
-    // extract STR details and get the second part of PGN
-    pgn.forEach((_, i, pgn) => pgn[i] = pgn[i].trim().replace('\r', ''));
-    for (let i = 0; i < pgn.length; i++) {
-        var line = pgn[i];
-        if (line[0] === '[') {
-            let tag = line.substring(1, line.indexOf(' '));
-            let value = line.substring(line.indexOf(' ') + 2, line.length - 2);
-            game.str[tag] = value;
-        } else if (line[0] !== '[' && line.trim() !== '') {
-            moves = pgn.slice(pgn.indexOf(line)).join(' ').trim().split('');
-            break;
-        }
-    }
-
-    // get move positions
-    var movPos = [];
-    var moveClone = moves.join('').split(' ');
-
+    movPos = [];
+    moveClone = moves.join('').split(' ');
     for (let i = 1; moveClone.includes(`${i}.`); i++) {
         movPos.push(moveClone.indexOf(`${i}.`));
     }
 
-    // extract NAGs and corresponding move
-    var temp = [];
-    moves = moves.join('').split(' ');
-    moves.pop();
-    game.nag = moves.filter(val => val.includes('$')).map(val => {
-        var prev = 0;
-        if (temp.length) {
-            prev = temp[temp.length - 1];
-        }
-        var count = movPos.filter(i => i < moves.indexOf(val, prev));
-        temp.push(count[count.length - 1]);
-        return {
-            moveCount: count.length,
-            value: val
-        }
-    });
-
-    // extract commentary and corresponding move
-    moves = moves.join(' ').split('');
-    while (moves.indexOf('{') !== -1) {
-        var commentStartPos = moves.indexOf('{');
-        var commentEndPos = moves.indexOf('}');
-        var comment = moves.splice(commentStartPos, commentEndPos - commentStartPos + 2).join('');
-        var commentIndex = moveClone.indexOf(comment.split(' ')[0]);
-        game.annotations.push({
-            moveCount: movPos.filter(ind => ind < commentIndex).length,
-            comment: comment.substring(1, comment.length - 2)
-        });
-    }
+    game.nag = getNags(moveClone, movPos);
+    game.annotations = getCommentary(moves, moveClone, movPos);
 
     moves = moves.join('').split(' ');
     moves = moves.filter(val => !val.includes('.') && !val.includes('$'));
+    moves.pop();
     game.moves = moves;
     return JSON.stringify(game, null, 4);
 }
