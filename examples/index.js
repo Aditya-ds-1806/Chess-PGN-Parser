67de3afe6bdc67d44d66b9c6094cae1677039293
Bebul <Bebul@users.noreply.github.com>
2022-01-28 16:35:50 +0100

    Support multiple games in pgn

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 const parser = require('chess-pgn-parser');
 const fs = require('fs');
 
-var pgn = fs.readFileSync('sample-1.pgn').toString();
+var pgn = fs.readFileSync('samples.pgn').toString();
 var json = parser.pgn2json(pgn);
 var obj = JSON.parse(json);
 
